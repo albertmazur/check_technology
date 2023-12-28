3c85dc93fe8d003bc5da626fa09d0ec02d3f09d8
albertmazur <albert153@interia.pl>
2023-12-28 08:05:26 +0100

    add Bing, Yahoo

--- searchUrlWithWordsKeyScraper.py
+++ searchUrlWithWordsKeyScraper.py
@@ -1,6 +1,6 @@
 import csv
 from urllib.parse import urlparse
-from search_engines import Google
+from search_engines import Google, Bing, Yahoo
 
 # Get user input for the search query, or use a default query
 user_query = input("Enter your search query (press Enter for default): ")
@@ -8,13 +8,26 @@ default_query = 'site:pl intitle:"sklep internetowy" OR intitle:"e-sklep" OR int
 search_query = user_query or default_query
 
 # Perform the Google search
-engine = Google()
-results = engine.search(search_query)
-links = results.links()
+google_engine = Google()
+google_results = google_engine.search(search_query)
+google_links = google_results.links()
+
+# Perform the Bing search
+bing_engine = Bing()
+bing_results = bing_engine.search(search_query)
+bing_links = bing_results.links()
+
+# Perform the Yahoo search
+yahoo_engine = Yahoo()
+yahoo_results = yahoo_engine.search(search_query)
+yahoo_links = yahoo_results.links()
+
+# Combine links from all search engines
+all_links = google_links + bing_links + yahoo_links
 
 # Extract unique domains from the links
 unique_domains = set()
-for link in links:
+for link in all_links:
     domain = urlparse(link).netloc
     if domain not in unique_domains:
         unique_domains.add(domain)
