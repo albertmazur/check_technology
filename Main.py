c039e83992c9c91ebfdcbfb256b25bc17ffe3d0b
Albert Mazur <albert153@interia.pl>
2024-05-27 11:38:40 +0200

    muliproccessing test

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import os
-
 from dotenv import load_dotenv
-
+from multiprocessing import Pool
 from FileHandler import FileHandler
 from Url import Url
 from technology import Joomla, Wordpress, PrestaShop, Magento
@@ -9,7 +8,6 @@ from technology import Joomla, Wordpress, PrestaShop, Magento
 
 def check(page_home_url):
     print(page_home_url)
-
     url = Url(page_home_url)
     response = url.create_request()
     if response.status_code != 404:
@@ -38,18 +36,22 @@ class Main:
         self.start_line = int(os.getenv("START_LINE"))
         self.count_line = int(os.getenv("COUNT_LINE"))
 
-        file_handler = FileHandler(self.path_input, self.path_output, self.start_line, self.count_line, self.column_url)
-        file_handler.writer_header_to_csv(
-            ["URL", "Technology", "Version WordPress", "IS WooCommerce", "Version WooCommerce", "Company", "E-mail"])
+        self.file_handler = FileHandler(self.path_input, self.path_output, self.start_line, self.count_line, self.column_url)
+        self.file_handler.writer_header_to_csv(
+            ["URL", "Technology", "Version WordPress", "IS WooCommerce", "Version WooCommerce", "Company", "E-mail"]
+        )
 
-        lines = file_handler.lines
+    def process_urls(self, urls):
         total_lines = self.start_line + self.count_line - 1
-        for index, url in enumerate(lines):
-            progress = ((index + 1) / self.count_line) * 100
-            data = check(url)
-            if data is not None:
-                file_handler.writer(data)
-            print(f"Przetworzono {self.start_line + index}/{total_lines} ({progress:.2f}%)")
+        with Pool(processes=os.cpu_count()) as pool:
+            results = pool.map(check, urls)
+            for index, data in enumerate(results):
+                progress = ((index + 1) / self.count_line) * 100
+                if data is not None:
+                    self.file_handler.writer(data)
+                print(f"Przetworzono {self.start_line + index}/{total_lines} ({progress:.2f}%)")
 
 
-Main()
+if __name__ == '__main__':
+    main_instance = Main()
+    main_instance.process_urls(main_instance.file_handler.lines)
